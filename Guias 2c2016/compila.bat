2d0ef8c9753f91457a9979bebd879c2cde5e0a5a
Diego Shalom <Diego Shalom>
2016-07-13 19:35:34 -0300

    Catedra, cuatri y anio ahora en un unico archivo con input

---
+++ Guias 2c2016/compila.bat
@@ -0,0 +1,12 @@
+pdflatex f2-guia1.2016
+pdflatex f2-guia2.2016
+pdflatex f2-guia3.2016
+pdflatex f2-guia4.2016
+pdflatex f2-guia5.2016
+pdflatex f2-guia6.2016
+pdflatex f2-guia7.2016
+pdflatex f2-guia8.2016
+pdflatex f2-guia9.2016
+pdflatex f2-guia10.2016
+pdflatex f2-guia11.2016
+
